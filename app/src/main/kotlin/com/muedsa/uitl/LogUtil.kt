847902adb26559de918250c880b7b57040026a2c
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-03-26 11:53:35 +0800

    fix: if Timber.d() `args` is empty array, throw UnknownFormatConversionException

--- app/src/main/kotlin/com/muedsa/uitl/LogUtil.kt
+++ app/src/main/kotlin/com/muedsa/uitl/LogUtil.kt
@@ -3,41 +3,40 @@ package com.muedsa.uitl
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 
-class LogUtil {
-    companion object {
-        @JvmStatic
-        fun d(message: String?, vararg args: Any?) {
-            Timber.d(message, args)
-        }
+object LogUtil {
 
-        @JvmStatic
-        fun d(t: Throwable?) {
-            Timber.d(t)
-        }
+    @JvmStatic
+    fun d(message: String) {
+        Timber.d(message)
+    }
 
-        @JvmStatic
-        fun d(t: Throwable?, message: String?, vararg args: Any?) {
-            Timber.d(t, message, args)
-        }
+    @JvmStatic
+    fun d(t: Throwable) {
+        Timber.d(t)
+    }
 
-        @JvmStatic
-        fun fb(message: String) {
-            Timber.d(message)
-            FirebaseCrashlytics.getInstance().log(message)
-        }
+    @JvmStatic
+    fun d(t: Throwable, message: String) {
+        Timber.d(t, message)
+    }
 
-        @JvmStatic
-        fun fb(t: Throwable) {
-            Timber.d(t)
-            FirebaseCrashlytics.getInstance().recordException(t)
-        }
+    @JvmStatic
+    fun fb(message: String) {
+        Timber.d(message)
+        FirebaseCrashlytics.getInstance().log(message)
+    }
 
-        @JvmStatic
-        fun fb(t: Throwable, message: String) {
-            Timber.d(t, message)
-            val crashlytics = FirebaseCrashlytics.getInstance()
-            crashlytics.log(message)
-            crashlytics.recordException(t)
-        }
+    @JvmStatic
+    fun fb(t: Throwable) {
+        Timber.d(t)
+        FirebaseCrashlytics.getInstance().recordException(t)
+    }
+
+    @JvmStatic
+    fun fb(t: Throwable, message: String) {
+        Timber.d(t, message)
+        val crashlytics = FirebaseCrashlytics.getInstance()
+        crashlytics.log(message)
+        crashlytics.recordException(t)
     }
 }
